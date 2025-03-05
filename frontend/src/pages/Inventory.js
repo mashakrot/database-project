3be1388c7c0f694cb30acc8a2665793c5d7b8e98
mashakrot <makrosensei@gmail.com>
2025-03-05 19:47:12 +0200

    corrected

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -114,79 +114,81 @@ export default function Inventory() {
   return (
     <div className="flex inventory-container">
       <Sidebar />
-      <div className="content p-6 w-full">
-        <h1 className="text-2xl font-bold">Inventory Management</h1>
-        <h2 className="font-bold mb-3">Update Inventory</h2>
-        <div className="update-container">
-          <div>
-            <label htmlFor="itemname">Item Name:</label>
-            <input
-              type="text"
-              id="itemname"
-              name="itemname"
-              placeholder="Enter item name to update"
-              value={searchValue}  
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
+      <div className="flex-1 ml-60 p-5">
+        <div className="p-6 w-full">
+          <h1 className="text-2xl font-bold">Inventory Management</h1>
+          <h2 className="font-bold mb-3">Update Inventory</h2>
+          <div className="update-container">
+            <div>
+              <label htmlFor="itemname">Item Name:</label>
+              <input
+                type="text"
+                id="itemname"
+                name="itemname"
+                placeholder="Enter item name to update"
+                value={searchValue}  
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            </div>
+            <div>
+              <label for="orderedQuantity">Ordered Quantity:</label>
+              <input
+                type="number"
+                id="orderedQuantity"
+                name="orderedQuantity"
+                placeholder="Enter quantity to add"
+                value={orderedQuantity}
+                onChange={(e) => setOrderedQuantity(Number(e.target.value))}
+              />
+            </div>
+            
+            <button className="btn" id="btn-update" onClick={handleUpdateQuantity}>Update Inventory</button>
           </div>
-          <div>
-            <label for="orderedQuantity">Ordered Quantity:</label>
-            <input
-              type="number"
-              id="orderedQuantity"
-              name="orderedQuantity"
-              placeholder="Enter quantity to add"
-              value={orderedQuantity}
-              onChange={(e) => setOrderedQuantity(Number(e.target.value))}
-            />
-          </div>
-          
-          <button className="btn" id="btn-update" onClick={handleUpdateQuantity}>Update Inventory</button>
-        </div>
-        <h2 className="font-bold mb-3">Inventory List</h2>
-        <table id="inventory-table">
-          <thead>
-            <tr>
-              <th>Item ID</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Category</th> {/* Renamed this for clarity */}
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {inventory.map(item => (
-              <tr key={item[0]}>
-                <td>{item[0]}</td> {/* Item ID */}
-                <td>{item[1]}</td> {/* Item Name */}
-                <td>{item[2]}</td> {/* Quantity */}
-                <td>{item[3]}</td> {/* Category */}
-                <td>${item[4]}</td> {/* Price */}
+          <h2 className="font-bold mb-3">Inventory List</h2>
+          <table id="inventory-table">
+            <thead>
+              <tr>
+                <th>Item ID</th>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Category</th> {/* Renamed this for clarity */}
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {inventory.map(item => (
+                <tr key={item[0]}>
+                  <td>{item[0]}</td> {/* Item ID */}
+                  <td>{item[1]}</td> {/* Item Name */}
+                  <td>{item[2]}</td> {/* Quantity */}
+                  <td>{item[3]}</td> {/* Category */}
+                  <td>${item[4]}</td> {/* Price */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
 
-        <h2 className="font-bold mb-3 mt-3">Search information about Suppliers</h2>
-        <div className="search-container">
-          <form id="formItemID" onSubmit={(e) => handleTrimmedSubmit(e, "itemid", "id")}>
-            <label htmlFor="itemid">Search by item ID:</label>
-            <input type="text" id="itemid" name="itemid"/>
-            {/* <input type="submit" id="button" value="Search"></input> */}
-            <button className="btn">Search</button>
-          </form>
+          <h2 className="font-bold mb-3 mt-3">Search information about Suppliers</h2>
+          <div className="search-container">
+            <form id="formItemID" onSubmit={(e) => handleTrimmedSubmit(e, "itemid", "id")}>
+              <label htmlFor="itemid">Search by item ID:</label>
+              <input type="text" id="itemid" name="itemid"/>
+              {/* <input type="submit" id="button" value="Search"></input> */}
+              <button className="btn">Search</button>
+            </form>
 
-          <form id="formItemName" onSubmit={(e) => handleTrimmedSubmit(e, "itemname", "name")}>
-            <label htmlFor="itemname">Search by item name:</label>
-            <input type="text" id="itemname" name="itemname"/>
-            {/* <input type="submit" value="Search"></input> */}
-            <button className="btn">Search</button>
-          </form>
+            <form id="formItemName" onSubmit={(e) => handleTrimmedSubmit(e, "itemname", "name")}>
+              <label htmlFor="itemname">Search by item name:</label>
+              <input type="text" id="itemname" name="itemname"/>
+              {/* <input type="submit" value="Search"></input> */}
+              <button className="btn">Search</button>
+            </form>
+          </div>
+          <label htmlFor="supplierData">Supplier Info:</label>
+          <input type="text" id="supplierData" readOnly />
         </div>
-        <label htmlFor="supplierData">Supplier Info:</label>
-        <input type="text" id="supplierData" readOnly />
+        </div>
       </div>
-    </div>
   );
 }
